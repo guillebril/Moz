51a91a48b9ec912360c87880d9e51a0634251dbd
Guille <guillebril@gmail.com>
2017-06-02 12:45:43 -0300

    React instalado con Binding :)

--- guest/src/App.js
+++ guest/src/App.js
@@ -1,36 +1,50 @@
 import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
-import firebase from 'firebase';
+import base from './rebase'
 
 class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      nombre: 'Guille',
       restaurantes: []
     };
   }
 
   componentDidMount() {
-    const rootRef = firebase.database().ref().child('restaurantes/');
-    rootRef.on('child_added', snap => {
-      console.log(snap.val().name)
+    // convierto el objeto a array y le hago a doble binding
+    base.syncState('restaurantes/', {
+      context: this,
+      state: 'restaurantes',
+      asArray: true
     });
   }
+
   render() {
+    //genero la lista y la guardo en una variable
+    var restaurantes = this.state.restaurantes.map((item, index) => {
+      return (
+        <p key={item.key}>
+          {item.name}</p>
+      )
+    })
+
+    //Pongo el resto del HTML
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo"/>
-          <p>Welcome {this.state.nombre}
-          </p>
+          <p>Welcome!</p>
         </div>
         <p className="App-intro">
           To get started, edit
           <code>src/App.js</code>
           and save to reload.
         </p>
+
+        <div>
+          {restaurantes}
+        </div>
       </div>
     );
   }
--- guest/src/index.js
+++ guest/src/index.js
@@ -2,16 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
-import * as firebase from 'firebase';
 
-var config = {
-  apiKey: "AIzaSyBTiGB3o_EzPxEIEBcDkaZVpWRgfdNQYOM",
-  authDomain: "mozo-c1716.firebaseapp.com",
-  databaseURL: "https://mozo-c1716.firebaseio.com",
-  projectId: "mozo-c1716",
-  storageBucket: "mozo-c1716.appspot.com",
-  messagingSenderId: "1094461273343"
-}
-firebase.initializeApp(config);
 ReactDOM.render(
   <App/>, document.getElementById('root'));
