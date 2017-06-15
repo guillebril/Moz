0603cd5cc9f279aebc7dbdb3eb5476ec5b6838a0
Guille <guillebril@gmail.com>
2017-06-14 22:26:06 -0300

    ddd

--- restaurante/src/App.js
+++ restaurante/src/App.js
@@ -1,44 +1,18 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import base from './rebase';
 
 class App extends Component {
-    constructor(props) {
-    super(props)
-    this.state = {
-      restaurantes: []
-    };
-  }
-    componentDidMount() {
-    // convierto el objeto a array y le hago a doble binding
-    base.syncState('restaurantes/', {
-      context: this,
-      state: 'restaurantes',
-      asArray: true
-    });
-  }
-    render() {
-    //genero la lista y la guardo en una variable
-    var restaurantes = this.state.restaurantes.map((item, index) => {
-      return (
-        <p key={item.key}>
-          {item.name}</p>
-      )
-    })
-
-
-  //Pongo el resto del HTML
+  render() {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo"/>
-          <p>Welcome!</p>
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to React</h2>
         </div>
-
-        <div>
-          {restaurantes}
-        </div>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
       </div>
     );
   }
