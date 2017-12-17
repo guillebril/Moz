0f1c3c6ea18263091e27741ee6c736423f7c6938
Guille <guillebril@gmail.com>
2017-12-17 11:47:30 -0300

    corrección de flexbox

--- guest/src/components/menu/productoItem.js
+++ guest/src/components/menu/productoItem.js
@@ -6,56 +6,54 @@ import ProductoSnackbar from './productoSnackbar'
 
 export default class Menuproducto extends Component {
 
+  constructor(props) {
 
-	constructor( props ) {
+    super(props)
+    this.state = {
+      open: false,
+      mostrarSnackbar: false,
+    }
+  }
 
+  handleClose = () => {
+    this.setState({
+      open: false,
+    });
+    createHistory().push('/')
+  };
 
-		super( props )
-		this.state = { open: false,
-										mostrarSnackbar: false,
-		 							}
-	}
+  handleCloseConSnackbar = () => {
+    this.setState({
+      open: false,
+      mostrarSnackbar: true
+    });
+    createHistory().push('/')
+    setTimeout(() => {
+      this.setState({
+        mostrarSnackbar: false
+      });
+    }, 3400);
+  };
 
+  handleOpen = () => {
+    this.props.estadoMesa !== 'abierta' ?
+      null :
+      this.setState({ open: true })
+  };
 
-	handleClose = ( ) => {
-		this.setState({ open: false,
-		 								});
-		createHistory().push('/' )
-		};
+  render() {
 
+    return (
 
-
-		handleCloseConSnackbar = () =>{
-			this.setState({ open: false,
-			 								mostrarSnackbar: true});
-			createHistory().push('/')
-			setTimeout( () => {
-				this.setState({
-				 								mostrarSnackbar: false});
-			},3400);
-			};
-
-
-
-
-			handleOpen = ( ) => {
-				this.props.estadoMesa !== 'abierta' ?
-				null
-				:
-				this.setState({ open: true })
-			};
-
-
-
-	render( ) {
-
-		return (
-
-			<Link to={'/'+this.props.producto.key}>
+      <Link to={'/'+this.props.producto.key}>
 				<div className='producto_item' onClick={this.handleOpen} >
-					<div className='producto_item_nombre'>{this.props.producto.nombre}</div>
-					<div className='producto_item_descripcion'>{this.props.producto.descripcion}</div>
-					<div className='producto_item_precio'>${this.props.producto.precio}</div>
+					<div className='producto_item_cont1'>
+						<div className='producto_item_nombre'>{this.props.producto.nombre}</div>
+						<div className='producto_item_descripcion'>{this.props.producto.descripcion}</div>
+					</div>
+
+
+				<div className='producto_item_precio'>${this.props.producto.precio}</div>
 					<div>
 
 						<Route path={'/'+this.props.producto.key} render={() =>
@@ -73,8 +71,7 @@ export default class Menuproducto extends Component {
 
 			</Link>
 
-
-		)
-	}
+    )
+  }
 
 }
--- guest/src/components/pestana.js
+++ guest/src/components/pestana.js
@@ -3,103 +3,86 @@ import React, { Component } from 'react';
 import base from '../rebase'
 
 import BottomNavigation, { BottomNavigationButton } from 'material-ui/BottomNavigation';
-import {LocalDining, LocalDrink} from 'material-ui-icons';
+import { LocalDining, LocalDrink } from 'material-ui-icons';
 import ListIcon from 'material-ui-icons/List';
 import Menu from './menu/menu';
 import Cuenta from './cuenta/cuenta'
 
-
-
 export default class Pestana extends Component {
 
-	state = {
-	value: 0,
-	bebidas:[],
-	comidas:[],
-	cuenta: {},
-	estadoMesa: '',
-};
+  state = {
+    value: 0,
+    bebidas: [],
+    comidas: [],
+    cuenta: {},
+    estadoMesa: '',
+  };
 
-		handleChange = (event, value) => {
-	     this.setState({ value });
-	   };
+  handleChange = (event, value) => {
+    this.setState({ value });
+  };
 
+  componentDidMount() {
+    base.syncState('restaurantes/oconnells/menu', {
+      context: this,
+      state: 'comidas',
+      asArray: true,
+      queries: {
+        orderByChild: 'tipoCategoria',
+        equalTo: 'Comidas',
+      }
 
+    });
 
-	 componentDidMount( ) {
- 		 base.bindToState('restaurantes/oconnells/menu', {
- 			 context: this,
- 			 state: 'comidas',
- 			 asArray: true,
-			 queries: {
-				 orderByChild: 'tipoCategoria',
-				 equalTo: 'Comidas',
-				 }
+    base.syncState('restaurantes/oconnells/menu', {
+      context: this,
+      state: 'bebidas',
+      asArray: true,
+      queries: {
+        orderByChild: 'tipoCategoria',
+        equalTo: 'Bebidas',
+      },
 
- 		 });
+    });
 
+    base.syncState('restaurantes/oconnells/mesas/-Kti1MOdTgkw0HwEH7Bh/pedidos', {
+      context: this,
+      state: 'cuenta',
+      asArray: true,
 
-		 base.syncState('restaurantes/oconnells/menu', {
-			 context: this,
-			 state: 'bebidas',
-			 asArray: true,
-			 queries: {
-				 orderByChild:  'tipoCategoria',
-				 equalTo: 'Bebidas',
-			 },
+    });
 
-		 });
-
-
-		 base.syncState('restaurantes/oconnells/mesas/-Kti1MOdTgkw0HwEH7Bh/pedidos', {
-			 context: this,
-			 state: 'cuenta',
-			 asArray: true,
-
-		 });
-
-		 base.syncState('restaurantes/oconnells/mesas/-Kti1MOdTgkw0HwEH7Bh/estadoMesa', {
-			context: this,
-			state: 'estadoMesa',
-			asArray: false,
-		});
-	 }
-
-
-
+    base.syncState('restaurantes/oconnells/mesas/-Kti1MOdTgkw0HwEH7Bh/estadoMesa', {
+      context: this,
+      state: 'estadoMesa',
+      asArray: false,
+    });
+  }
 
   ventana = () => {
-		const categoriasComidasOrdenadas = [].concat(this.state.comidas).sort((a, b) => a.pos > b.pos)
-		const categoriasBebidasOrdenadas = [].concat(this.state.bebidas).sort((a, b) => a.pos > b.pos)
-		//verifico si las categorias del menu con sus productos ya llego.
-		//const cargando = Object.keys(categoriasComidasOrdenadas[0]).length === 0
+    const categoriasComidasOrdenadas = [].concat(this.state.comidas).sort((a, b) => a.pos > b.pos)
+    const categoriasBebidasOrdenadas = [].concat(this.state.bebidas).sort((a, b) => a.pos > b.pos)
 
+    if (this.state.value === 0) {
+      return (
+        <Menu estadoMesa={this.state.estadoMesa} menu={categoriasBebidasOrdenadas} />
+      )
+    }
+    if (this.state.value === 1) {
 
+      return (<Menu estadoMesa={this.state.estadoMesa} menu={categoriasComidasOrdenadas}/>)
+    } else {
 
- 		if (this.state.value === 0){
-			return (
-			 <Menu estadoMesa={this.state.estadoMesa} menu={categoriasBebidasOrdenadas} />
-		 )}
-		 if (this.state.value === 1){
+      return (
+        <Cuenta estadoMesa={this.state.estadoMesa} cuenta={this.state.cuenta}/>
+      )
+    }
 
-			 return (<Menu estadoMesa={this.state.estadoMesa} menu={categoriasComidasOrdenadas}/>)
-		 } else {
+  }
 
-				return (
-						<Cuenta estadoMesa={this.state.estadoMesa} cuenta={this.state.cuenta}/>
-					)
-		 }
-
-
- }
-
-
-
-
-
-	render(){
+  render() {
     return (
-			<div>
+      <div>
 				<div style={{marginBottom: '64px'}}>
 					{this.ventana()}
 				</div>
@@ -116,4 +99,4 @@ export default class Pestana extends Component {
     );
   }
 
-	}
+}
